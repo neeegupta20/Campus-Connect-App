abe244ce4852f0ddccab411f4ad3d93dc54ccc9e
neeegupta20 <152726313+neeegupta20@users.noreply.github.com>
2025-01-30 15:08:26 +0530

    Update connectZonesTab.tsx

--- frontend/app/connectZonesTab.tsx
+++ frontend/app/connectZonesTab.tsx
@@ -6,65 +6,59 @@ import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-na
 import Entypo from '@expo/vector-icons/Entypo';
 import ConnectZone from './connectZoneType';
 
-  const {height:SCREEN_HEIGHT}=Dimensions.get('window')
-  const TAB_BAR_HEIGHT=950;
+const {height:SCREEN_HEIGHT}=Dimensions.get('window')
+const TAB_BAR_HEIGHT=950;
   
-  const BottomSlider:React.FC<{isOpen:boolean,zone:ConnectZone|null,onClose:()=>void}>=({isOpen,zone,onClose})=>{
+const BottomSlider:React.FC<{isOpen:boolean,zone:ConnectZone|null,onClose:()=>void}>=({isOpen,zone,onClose})=>{
     
-    const translateY=useSharedValue(SCREEN_HEIGHT);
-    const context=useSharedValue({y:0});
+  const translateY=useSharedValue(SCREEN_HEIGHT);
+  const context=useSharedValue({y:0});
 
-    const gesture=Gesture.Pan()
-      .onStart(() => {
-        context.value = { y: translateY.value };
-      })
-      .onUpdate((event) => {
-        translateY.value=event.translationY+context.value.y;
-        translateY.value=Math.max(translateY.value,-SCREEN_HEIGHT+100);
-        translateY.value=Math.min(translateY.value, SCREEN_HEIGHT-TAB_BAR_HEIGHT);
-      })
-      .onEnd(() => {
-        if (translateY.value > SCREEN_HEIGHT / 2) {
-          translateY.value = withSpring(SCREEN_HEIGHT); 
-        } else {
-          translateY.value = withSpring(-SCREEN_HEIGHT / 3); 
-        }
-      })
-      useEffect(() => {
-        translateY.value = withSpring(isOpen ? -SCREEN_HEIGHT / 3 : SCREEN_HEIGHT-TAB_BAR_HEIGHT, { damping: 50 });
-      }, [isOpen]);
+  const gesture=Gesture.Pan()
+    
+  .onStart(()=>{
+    context.value={y:translateY.value};
+  })
+    
+  .onUpdate((event)=>{
+    translateY.value=event.translationY+context.value.y;
+    translateY.value=Math.max(translateY.value,-SCREEN_HEIGHT+100);
+    translateY.value=Math.min(translateY.value, SCREEN_HEIGHT-TAB_BAR_HEIGHT);
+  })
+    
+  useEffect(()=>{
+    translateY.value=withSpring(isOpen?-SCREEN_HEIGHT/3 : SCREEN_HEIGHT-TAB_BAR_HEIGHT, {damping:50});
+  },[isOpen]);
 
-      const handleSlider = () => {
-        translateY.value = withSpring(SCREEN_HEIGHT-TAB_BAR_HEIGHT)
-        onClose()
-      }
+  const handleSlider=()=>{
+    translateY.value=withSpring(SCREEN_HEIGHT-TAB_BAR_HEIGHT)
+    onClose()
+  }
 
-      const rBottomSlider=useAnimatedStyle(()=>{
-        return {
-          transform:[{ translateY: translateY.value }],
-        };
-      });
+  const rBottomSlider=useAnimatedStyle(()=>{
+    return {
+      transform:[{ translateY: translateY.value }],
+    };
+  });
 
-
-
-    return (
-      <GestureDetector gesture={gesture}>
-        <Animated.View style={[styles.bottomSliderContainer,rBottomSlider]}>
-            <View style={styles.line}/>
-            <View>
-              <TouchableOpacity style={styles.sliderCross} onPress={handleSlider}>
-                <Entypo name="cross" size={24} color="grey" />
-              </TouchableOpacity>
-              {zone &&
-                <>
-                  <Text style={styles.zoneName}>{zone.name}</Text>
-                  <Text style={styles.zoneDescription}>{zone.description}</Text>
-                </>
-              }
-            </View>
-        </Animated.View>
-      </GestureDetector>
-    );
+  return (
+    <GestureDetector gesture={gesture}>
+      <Animated.View style={[styles.bottomSliderContainer,rBottomSlider]}>
+          <View style={styles.line}/>
+          <View>
+            <TouchableOpacity style={styles.sliderCross} onPress={handleSlider}>
+              <Entypo name="cross" size={24} color="grey" />
+            </TouchableOpacity>
+            {zone &&
+              <>
+                <Text style={styles.zoneName}>{zone.name}</Text>
+                <Text style={styles.zoneDescription}>{zone.description}</Text>
+              </>
+            }
+          </View>
+      </Animated.View>
+    </GestureDetector>
+  );
 };
 
 
