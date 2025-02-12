42a04af2d09aa58fc745282843b71b01cd162eba
neeegupta20 <152726313+neeegupta20@users.noreply.github.com>
2025-02-12 17:22:26 +0530

    Update _layout.tsx

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,29 +1,27 @@
 import { useContext, useEffect, useState } from "react";
 import { Slot, useRouter } from "expo-router";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView } from "react-native";
 import UserProvider, { UserContext } from "./context/UserContext";
 import { StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
 import SplashScreen from "@/components/SplashScreen";
 import LottieView from "lottie-react-native";
 
-function RootLayoutInner(){
-    
-    const { user, fetchUserProfile }=useContext(UserContext);
-    const router=useRouter();
-    const [hasCheckedAuth, setHasCheckedAuth]=useState(false);
-    const [isLoading,setIsLoading]=useState(true);
+function RootLayoutInner() {
+    const { user, fetchUserProfile } = useContext(UserContext);
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasCheckedAuth, setHasCheckedAuth] = useState(false);
 
-    useEffect(()=>{
-        const timeout=setTimeout(()=>{
+    useEffect(() => {
+        const timeout = setTimeout(() => {
             setIsLoading(false);
-        },4250);
-        return ()=>clearTimeout(timeout);
+        }, 4250);
+        return () => clearTimeout(timeout);
     }, []);
 
-    useEffect(()=>{
-        if (!isLoading){
-            const initializeAuth=async()=>{
+    useEffect(() => {
+        if (!isLoading) {
+            const initializeAuth = async () => {
                 await fetchUserProfile();
                 setHasCheckedAuth(true);
             };
@@ -31,51 +29,55 @@ function RootLayoutInner(){
         }
     }, [isLoading]);
 
-    useEffect(()=>{
+    useEffect(() => {
         if (hasCheckedAuth) {
-            if(user){
-                router.replace("/(tabs)");
-            }else{
-                router.replace("/(auth)");
-            }
+            setTimeout(() => {
+                if (user) {
+                    router.replace("/(tabs)");
+                } else {
+                    router.replace("/(auth)");
+                }
+            },250);
         }
-    },[user,hasCheckedAuth]);
+    }, [hasCheckedAuth]);
 
-    if (isLoading) {
-        return <SplashScreen />;
+    if(isLoading){
+        return <SplashScreen/>;
     }
 
     if (!hasCheckedAuth){
-        return(
+        return (
             <SafeAreaView style={styles.errorContainer}>
-                <LottieView source={"frontend/assets/loaderWhite.json"} autoPlay loop style={styles.loaderIcon}/>
+                <LottieView
+                    source={require("../assets/loaderWhite.json")}
+                    autoPlay
+                    loop
+                    style={styles.loaderIcon}
+                />
             </SafeAreaView>
-        )
+        );
     }
 
     return <Slot/>;
 }
 
-export default function RootLayout(){
+export default function RootLayout() {
     return (
         <UserProvider>
-            <RootLayoutInner/>
+            <RootLayoutInner />
         </UserProvider>
     );
 }
 
-const styles=StyleSheet.create(
-    {
-        errorContainer:{
-            alignItems:'center',
-            marginTop:350,
-            justifyContent: "center"
-        },
-        loaderIcon:{
-            width: 60,
-            height: 60,
-            alignSelf:"center",
-            top: 300
-        }
-    }
-);
+const styles=StyleSheet.create({
+    errorContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    loaderIcon: {
+        width: 60,
+        height: 60,
+        alignSelf: "center",
+        top: 300,
+    },
+});
