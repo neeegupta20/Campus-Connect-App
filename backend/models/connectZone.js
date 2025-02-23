cf6cd9b3871793fcf1cec114283dee5d8ddbd80a
PunyaMutreja <punyamutreja2@gmail.com>
2025-02-23 15:22:45 +0530

    Update connectZone.js

--- backend/models/connectZone.js
+++ backend/models/connectZone.js
@@ -1,7 +1,31 @@
 const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://admin:campusconnect12%40@campusconnect.pahyh.mongodb.net/CampusConnectApp");
 
-const connectZone = new mongoose.Schema({
-    id:
+const connectZoneSchema = new mongoose.Schema({
+    id:{
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    latitude: {
+        type: Number,
+        required: true
+    },
+    longitude: {
+        type: Number,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
 })
-module.exports=mongoose.model("Notifications",notifications)
+module.exports=mongoose.model("connectZone",connectZoneSchema)
+
