9b3c13461be03de05ea3d1add01216284db45e8c
neeegupta20 <152726313+neeegupta20@users.noreply.github.com>
2025-07-31 23:33:00 +0530

    Update editAvatar.tsx

--- frontend/app/(account)/editAvatar.tsx
+++ frontend/app/(account)/editAvatar.tsx
@@ -69,8 +69,7 @@ const styles=StyleSheet.create({
   backIcon:{
     zIndex:20,
     padding:20,
-    top: Platform.OS==="android"?30:70,
-    // marginTop: Platform.OS==="android"?30:70,
+    top: Platform.OS==="ios"?0:70,
   },
   headText:{
     color:"white",
